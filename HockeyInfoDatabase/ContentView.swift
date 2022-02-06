bd2eb019b2a432bee3677d2f91c801e6d5a2db52
Larry Burris <lburris@mac.com>
2022-02-05 23:17:43 -0500

    Initial Commit

---
+++ HockeyInfoDatabase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HockeyInfoDatabase
+//
+//  Created by Larry Burris on 2/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HockeyInfoDatabase/HockeyInfoDatabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  HockeyInfoDatabaseApp.swift
+//  HockeyInfoDatabase
+//
+//  Created by Larry Burris on 2/5/22.
+//
+
+import SwiftUI
+
+@main
+struct HockeyInfoDatabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
